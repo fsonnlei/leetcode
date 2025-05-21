e4e660bde46a2214d4e37ea5a5bfecab091be43c
fsonnlei <fsonnlei@yahoo.com>
2025-05-21 15:28:32 -0700

    leetcode 88

--- leetcode-88.py
+++ leetcode-88.py
@@ -2,17 +2,17 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
     """
     Do not return anything, modify nums1 in-place instead.
     """
-    print(nums1, nums2)
     nums1[m:m+n] = nums2
     nums1.sort()
-    print(nums1)
 
 
 nums1 = [1,2,3,0,0,0]
 m = 3
 nums2 = [2,5,6]
 n = 3
+print(nums1, nums2)
 merge(nums1, m, nums2, n)
+print(nums1)
 
 nums1 = [1]
 m = 1
